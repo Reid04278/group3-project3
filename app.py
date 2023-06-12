5a8b085ecc1c3406cfbb6b7bfaa1561332953759
adinger1 <anikadinger512@gmail.com>
2023-06-12 15:10:03 -0400

    Updated Flask setup, still in progress

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def create_cursor():
         host='localhost',
         database='project3',
         user='postgres',
-        password='*BlackSkies7'
+        password='Gwqcmn47sv'
     )
 
     # Create a cursor to interact with the database
@@ -36,29 +36,41 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/percent_price_changes<br/>"
+        #f"/api/v1.0/percent_price_changes<br/>"
         f"/api/v1.0/domestic_change<br/>"
-        f"/api/v1.0/international_food_price_data"
+        f"<br/>"
+        f"To see the changing international food prices over time for a specific country, enter the following route and replace 'country_name' with the country of interest (case sensitive)<br/>"
+        f"/api/v1.0/international_food_price_data/country_name<br/>"
+        f"<br/>"
+        f"To see the full list of available countries with international food price data, go to the following route:<br/>"
+        f"/api/v1.0/country_list<br/>"
+        f"<br/>"
     )
 
-@app.route("/api/v1.0/percent_price_changes")
-def percent_price_changes():
+# @app.route("/api/v1.0/percent_price_changes")
+# def percent_price_changes():
     
-    conn, cursor = create_cursor()
-    cursor.execute('SELECT country,commodity,post_covid FROM percent_change_post_covid')
-    #percentage_display_data = db.session.query(percent_change.country,percent_change.commodity,percent_change.post_covid).all()
+#     conn, cursor = create_cursor()
+#     cursor.execute('SELECT * FROM percent_change_post_covid')
+#     #percentage_display_data = db.session.query(percent_change.country,percent_change.commodity,percent_change.post_covid).all()
 
-    percentage_display_data = cursor.fetchall()
+#     percentage_display_data = cursor.fetchall()
     
-    # Close the cursor and the database connection
-    cursor.close()
-    conn.close()
+#     cursor.execute("SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('percent_change_post_covid')")
     
-    return jsonify(percentage_display_data)
+#     column_names = cursor.fetchall()
+    
+    
+#     # Close the cursor and the database connection
+#     cursor.close()
+#     conn.close()
+    
+    
+#     return jsonify(percentage_display_data)
 
 
     
-@app.route("/api/v1.0/domestic_change")
+@app.route("/api/v1.0/domestic_change/<country>")
 def domestic_change():
     conn, cursor = create_cursor()
     
@@ -70,22 +82,41 @@ def domestic_change():
     cursor.close()
     conn.close()
     
-    return jsonify(domestic_data)
- 
-@app.route("/api/v1.0/international_food_price_data")
-def international_data():
+    str = "Let's see if this string prints"
+    return jsonify(str, domestic_data)
+
+@app.route("/api/v1.0/international_food_price_data/<country>")
+def international_data(country):
     conn, cursor = create_cursor()
     
-    cursor.execute('SELECT * FROM int_clean_data_cleaned')
+    query = f"SELECT * FROM int_clean_data_cleaned WHERE country = '{country}' ORDER BY commodity,time"
+    cursor.execute(query)
 
     international_data = cursor.fetchall()
+    
+    cursor.execute(f"SELECT DISTINCT commodity FROM int_clean_data_cleaned WHERE country = '{country}'")
+    commodities = cursor.fetchall()
+    # Close the cursor and the database connection
+    cursor.close()
+    conn.close()
+    
+    return jsonify(commodities,international_data)
+
+
+@app.route("/api/v1.0/country_list")
+def country_list():
+    conn, cursor = create_cursor()
+    
+    query = f"SELECT DISTINCT country FROM int_clean_data_cleaned"
+    cursor.execute(query)
+    
+    countries = cursor.fetchall()
     
     # Close the cursor and the database connection
     cursor.close()
     conn.close()
     
-    return jsonify(international_data)
-
+    return jsonify(countries)
 
 
 if __name__ == "__main__":

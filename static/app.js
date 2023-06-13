eef9c5916f938611490c2e006d0bdb003f03f53d
John Kelly <hammerfailed92@gmail.com>
2023-06-13 14:17:09 -0400

    Changes to be committed: 	modified:   static/app.js

--- static/app.js
+++ static/app.js
@@ -6,6 +6,79 @@ function updateSankey(country){
 }
 
 function updateBoxplot(country){
+  /*
+  // Data for the box plot
+const data = [
+  [12, 14, 16, 18, 20, 21, 23],
+  [10, 12, 15, 17, 19, 20, 22],
+  [8, 11, 14, 16, 18, 19, 21]
+];
+
+// Dimensions for the chart
+const width = 500;
+const height = 300;
+const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+// Create the SVG container
+const svg = d3.select("#chart")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Create the scales
+const xScale = d3.scaleBand()
+  .domain(data.map((_, i) => i))
+  .range([0, width])
+  .paddingInner(0.1);
+
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(data.flat())])
+  .range([height, 0]);
+
+// Create the box plot
+svg.selectAll("g")
+  .data(data)
+  .enter()
+  .append("g")
+  .attr("transform", (_, i) => `translate(${xScale(i)},0)`)
+  .selectAll("rect")
+  .data(d => d3.boxPlotQuartiles(d))
+  .enter()
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", d => yScale(d[2]))
+  .attr("width", xScale.bandwidth())
+  .attr("height", d => yScale(d[0]) - yScale(d[2]))
+  .attr("stroke", "black")
+  .attr("fill", "transparent");
+
+// Add the median line
+svg.selectAll("g")
+  .data(data)
+  .selectAll("line")
+  .data(d => [d3.boxPlotMedian(d)])
+  .enter()
+  .append("line")
+  .attr("x1", 0)
+  .attr("y1", d => yScale(d))
+  .attr("x2", xScale.bandwidth())
+  .attr("y2", d => yScale(d))
+  .attr("stroke", "black");
+
+// Add the whiskers
+svg.selectAll("g")
+  .data(data)
+  .selectAll("line")
+  .data(d => [d3.boxPlotWhiskers(d)])
+  .enter()
+  .append("line")
+  .attr("x1", 0)
+  .attr("y1", d => yScale(d[0
+
+
+
+  */
 
 }
 
@@ -40,7 +113,58 @@ function updateLinegraphs(data, country){
 }
 
 
-function updateBarCharts(country){
+function updatestackedBarCharts(country){
+  /*
+example
+const data = {
+  labels: ['Category 1', 'Category 2', 'Category 3'],
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [10, 20, 30],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)'
+    },
+    {
+      label: 'Dataset 2',
+      data: [15, 25, 35],
+      backgroundColor: 'rgba(54, 162, 235, 0.5)'
+    },
+    {
+      label: 'Dataset 3',
+      data: [5, 15, 25],
+      backgroundColor: 'rgba(75, 192, 192, 0.5)'
+    }
+  ]
+};
+
+// Chart configuration
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    responsive: true,
+    scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true
+      }
+    }
+  }
+};
+
+// Create the chart
+const ctx = document.getElementById('myChart').getContext('2d');
+new Chart(ctx, config)
+
+
+  */
+
+
+
+
+
 
 }
 
